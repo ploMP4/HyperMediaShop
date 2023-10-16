cf7953a56445cf49b3f5e79702f2466a0075c9d6
plo <kostasarto2@gmail.com>
2023-10-17 01:54:01 +0300

    feat: add tailwind css to the project

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 build:
+	npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css
 	templ generate -path ./templates/ 
 	docker-compose build 
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.templ", "./templates/**/*.templ"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
