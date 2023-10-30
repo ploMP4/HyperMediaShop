a6c1e001a38aa511c255d25aa51452bcf40a3ffa
ploMP4 <kostasarto2@gmail.com>
2023-10-30 18:13:11 +0200

    feat: update product model

--- models/product.go
+++ models/product.go
@@ -1,12 +1,16 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 )
 
 type Product struct {
-	ID        string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID          string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Name        string `gorm:"not null"`
+	Description sql.NullString
+	ImgURL      string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"`
+	Price       int    `gorm:"not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
