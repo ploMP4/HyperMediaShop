6e23fd4ccdd0fdafbde617f8824109a1ce70fbe7
ploMP4 <kostasarto2@gmail.com>
2023-11-26 00:14:32 +0200

    feat: add more image fields to product model

--- models/product.go
+++ models/product.go
@@ -1,14 +1,20 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type Product struct {
 	BaseModel
 
-	Name        string `gorm:"not null;type:varchar(150)"`
-	Description sql.NullString
-	ImgURL      string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"`
-	Price       int    `gorm:"not null"`
+	Name           string `gorm:"not null;type:varchar(150)"`
+	Description    sql.NullString
+	PreviewImgURL  string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"`
+	DisplayImgURL1 string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg"`
+	DisplayImgURL2 string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg"`
+	DisplayImgURL3 string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg"`
+	DisplayImgURL4 string `gorm:"default:https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg"`
+	Price          int    `gorm:"not null"`
 
 	Categories []*Category `gorm:"many2many:product_category"`
 	Reviews    []Review
